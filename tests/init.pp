fd9557ec8f909d2ad5c6d7daa2f7410bb7aa48fe
Andrew Brader <abrader@gmail.com>
2014-11-05 12:03:48 -0500

    Added extra example
    Wanted to show an ensure => absent example.

--- tests/init.pp
+++ tests/init.pp
@@ -18,4 +18,12 @@ class gms {
     server_url   => 'http://glsrv.puppetlabs.vm',
   }
   
+  git_deploy_key { 'should_be_removed' :
+    ensure       => absent,
+    path         => '/home/abrader/.ssh/id_rsa.pub',
+    token        => 'NcuHJ7MLkJnx2DZJ5MKn',
+    project_name => 'abrader/control',
+    server_url   => 'http://glsrv.puppetlabs.vm',
+  }
+  
 }
