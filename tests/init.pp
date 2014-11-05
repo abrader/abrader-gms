1a773c279fd5c84fbd2ae0690173999dd4a7d8e9
Andrew Brader <abrader@gmail.com>
2014-11-05 12:00:16 -0500

    Update init.pp
    Added examples on how to call the git_deploy_key type.

--- tests/init.pp
+++ tests/init.pp
@@ -1,2 +1,22 @@
-include gitlab
+class gms {
 
+  git_deploy_key { 'abrader_macbookpro_key_gitlab.com' :
+    ensure       => present,
+    name         => 'abrader_macbookpro_key',
+    path         => '/root/.ssh/id_rsa.pub',
+    token        => 'Y5E8vXdjhTu6aDp3YRWs',
+    project_id   => 110384,
+    project_name => '',
+    server_url   => 'https://gitlab.com',
+  }
+  
+  git_deploy_key { 'abrader_macbookpro_key_glsrv.puppetlabs.vm' :
+    ensure       => present,
+    name         => 'abrader_macbookpro_key',
+    path         => '/home/abrader/.ssh/id_rsa.pub',
+    token        => 'NcuHJ7MLkJnx2DZJ5MKn',
+    project_name => 'abrader/control',
+    server_url   => 'http://glsrv.puppetlabs.vm',
+  }
+  
+}
