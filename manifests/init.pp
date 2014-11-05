92632cd81022ea4153c39bc25f4e0df21968e44a
Andrew Brader <abrader@gmail.com>
2014-11-05 11:44:04 -0500

    Update example init.pp
    Made example more clear by

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,14 @@
 class gms(
+  $name         = <unique name to identify key>
   $path         = <absolute path to SSH key>,
   $token        = <token>,
   $project_id   = <project_id>,
   $project_name = <project/respository name>,
-  $user_id      = <user id>,
-  $access_level = <GUEST = 10, REPORTER = 20, DEVELOPER = 30, MASTER = 40, OWNER = 50>,
-  $sshkey_title = <title for test ssh key>,
   $server_url   = <URL for Git management system # i.e., https://gitlab.com>,
 ) {
 
   git_deploy_key { 'unique_key_name' :
+    name         => $name,
     ensure       => present,
     path         => '/root/.ssh/id_rsa.pub',
     token        => $token,
